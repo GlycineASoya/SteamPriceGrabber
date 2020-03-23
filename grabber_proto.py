47622f4963bc017ab2b16c9aedca5c0818745fd3
Ilya Levinsky <Ilya Levinsky>
2020-03-24 00:03:58 +0300

    Added parsing of the name, added procedures to find price and find a name

--- grabber_proto.py
+++ grabber_proto.py
@@ -1,20 +1,25 @@
+from typing import Optional, Match
+
 import requests
 import re
 
+def find_name(text: str) -> str:
+    match: Optional[Match[object]] = re.search('itemprop=\"name\">', text)
+    return text[match.end():re.compile('</').search(text, match.end()).start()].lstrip().rstrip()
+
+def find_price(text: str) -> int:
+    if "discount_final_price" in text:
+        match: Optional[Match[object]] = re.search('discount_final_price\">', text)
+    else:
+        match: Optional[Match[object]] = re.search('game_purchase_price price\">', text)
+    return text[match.end():re.compile('</').search(text, match.end()).start()].lstrip().rstrip()
+
+
 URL = "http://store.steampowered.com/app/440/Team_Fortress_2/"
+URL = "http://store.steampowered.com/app/435150/Divinity_Original_Sin_2__Definitive_Edition/"
 r = requests.get(url=URL)
-# data = json.dumps(r.text)
-match = re.search('game_purchase_price price\">', r.text)
-end = match.end()
-# print(pos)
 
-# big1 = re.compile('</div>').search(r.text, pos).start()
-# print(big1)
-
-
-price = r.text[end:re.compile('</div>').search(r.text, end).start()].lstrip().rstrip()
-
-print(price)
+print(find_name(r.text), find_price(r.text))
 
 '''
 with open('url.json', 'w') as file:
