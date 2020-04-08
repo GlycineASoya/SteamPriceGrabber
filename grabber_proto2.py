6b381d8555bdec9b759c0a4ff835a79dc35b1f1d
Ilia Levinskii <ivlevinsk@gmail.com>
2020-04-08 23:22:35 +0300

    Added bundle getting procedure; improved title getting procedure

--- grabber_proto2.py
+++ grabber_proto2.py
@@ -20,9 +20,14 @@ def getPage(url: str) -> requests.models.Response:
     return requests.get(url, cookies={"birthtime": "0"})
 
 
-def getIdList(page: requests.models.Response) -> list:
-    ids = list(re.compile(r"app[\/]([0-9]+)").findall(page.text.partition(r"<!-- List Items -->")[2]))
-    return ids
+def getBundleList(page: requests.models.Response) -> list:
+    bundles = list(re.compile(r"bundle[\/]([0-9]+)").findall(page.text.partition(r"<!-- List Items -->")[2]))
+    return bundles
+
+
+def getAppList(page: requests.models.Response) -> list:
+    apps = list(re.compile(r"app[\/]([0-9]+)").findall(page.text.partition(r"<!-- List Items -->")[2]))
+    return apps
 
 
 def getPlatformList(page: requests.models.Response) -> list:
@@ -32,13 +37,14 @@ def getPlatformList(page: requests.models.Response) -> list:
 
 
 def getTitleList(page: requests.models.Response) -> list:
-    titles = list(re.compile(r"title.>([\w\S][^<]+)").findall(page.text.partition(r"<!-- List Items -->")[2]))
+    titles = list(re.compile(r"title.>([^<]+|)").findall(page.text.partition(r"<!-- List Items -->")[2]))
     return titles
 
 
 def getPriceList(page: requests.models.Response) -> list:
-    prices = list(set(re.compile(r"search_price.+\s+([\d,]+[\S]+)|>([\d,]+[\S]+)<\/strike").findall(
-        page.text.partition(r"<!-- List Items -->")[2])))
+    prices = list(
+        set(re.compile(r"search_price[^_].+\s+([\d,]+[\d]+[\S]+)|(Free\s+to\s+Play)|>([\d,]+[\S]+)<\/strike").findall(
+            page.text.partition(r"<!-- List Items -->")[2])))
     result = map(lambda x: x[0] if (x[1] == '') else x[1], prices)
     return list(result)
 
@@ -55,20 +61,21 @@ def getDiscountList(page: requests.models.Response) -> list:
     return discounts
 
 
-main_url = r"http://store.steampowered.com/search/?ignore_preferences=1&sort_by=_ASC"  # &page=
+main_url = r"http://store.steampowered.com/search/?ignore_preferences=1&sort_by=Name_ASC"  # &page=
 game_list = list()
 cc_price = list()
 
 # title.>([\w\S][^<]+)
 
 
-if isPageExist(main_url):
+if isPageExist(main_url + "&page=1"):
     page = getPage(main_url)
     # last_page = re.compile(r";(\d+)\s+").search(page.text)
     # for page_number in range(last_page):
-    print("IDs:\t" + "\t".join(getIdList(page)))
-    print("Titles:\t" + "\t".join(getTitleList(page)))
-    #print("Platforms: " + "\t".join(getPlatformList(page)))
-    print("Discounts:\t" + "\t".join(getDiscountList(page)))
-    print("Discount prices:\t" + "\t".join(getDiscountPriceList(page)))
-    print("Prices:\t" + "\t".join(getPriceList(page)))
+    print("Apps :\t" + "\t".join(str(len(getAppList(page)))))
+    print("Bundles :\t" + "\t".join(str(len(getBundleList(page)))))
+    print("Titles:\t" + "\t> ".join((getTitleList(page))))
+    # print("Platforms: " + "\t".join(getPlatformList(page))))
+    print("Discounts:\t" + "\t".join(str(len(getDiscountList(page)))))
+    print("Discount prices:\t" + "\t".join(str(len(getDiscountPriceList(page)))))
+    print("Prices:\t" + "\t".join(str(len(getPriceList(page)))))
